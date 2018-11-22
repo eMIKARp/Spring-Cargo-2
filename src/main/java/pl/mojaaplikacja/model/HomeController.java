d286c7883a96093020b1d9aca024286fc60fe847
eMIKARp <emil.karpowicz@gmail.com>
2018-11-22 22:46:39 +0100

    Initial commit

---
+++ src/main/java/pl/mojaaplikacja/model/HomeController.java
@@ -0,0 +1,16 @@
+package pl.mojaaplikacja.model;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {
+		
+		return "Welcome to webapp deployed by Jenkins";
+	}
+}
